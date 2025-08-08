c6efa2f96485c380c3b81ccecd4229961226094d
Sylvanus-the-sorcerer <103826382+sepehrsylvanus@users.noreply.github.com>
2025-08-08 21:31:35 +0300

    remove extra logs

--- actions/auth.js
+++ actions/auth.js
@@ -5,13 +5,11 @@ import { cookies } from "next/headers";
 
 export const getToken = async (initData) => {
   const cookieStore = await cookies();
-  console.log("🚀 ~ getToken ~ initData:", initData);
   try {
     const res = await AXIOS.post("/register", { initData });
     const token = res.data.data.token;
     cookieStore.set("token", token);
   } catch (error) {
-    console.log("🚀 ~ getToken ~ error:", error);
     console.log(error.message);
     throw new Error(error.message);
   }
